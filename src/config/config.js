59bce004327773daf574fc9b6a991b36f96b383d
bnloading <nurbakhitjan5@gmail.com>
2025-06-04 15:31:38 +0500

    An auisti

--- src/config/config.js
+++ src/config/config.js
@@ -35,7 +35,7 @@ const config = {
       },
     ],
     audio: {
-      src: "/audio/toi2.mp3",
+      src: "/audio/toi1.mp3",
       title: "Көңілді әуен",
       autoplay: true,
       loop: true,
